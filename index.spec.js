ae6f732916ea720f4b8def8515a2414d5993572a
moo25 <ansckdan@gmail.com>
2018-04-27 22:27:31 +0900

    user revise API(fail)

--- index.spec.js
+++ index.spec.js
@@ -4,7 +4,7 @@ const app = require('./index')
 
 describe('GET /users는', () => {
   describe(' 성공시', () => {
-    it('1. 유저 객체를 담은 배열로 응답함', (done) => {
+    it('유저 객체를 담은 배열로 응답함', (done) => {
       request(app)
         .get('/users')
         .end((err, res) => {
@@ -12,7 +12,7 @@ describe('GET /users는', () => {
           done()
         })
     })
-    it('2. 최대 limit 갯수만큼 응답한다', (done) => {
+    it('최대 limit 갯수만큼 응답한다', (done) => {
       request(app)
         .get('/users?limit=2')
         .end((err, res) => {
@@ -22,7 +22,7 @@ describe('GET /users는', () => {
     })
   })
   describe('실패시', () => {
-    it('1. limit이 숫자형이 아니면 400을 응답한다', (done) => {
+    it('limit이 숫자형이 아니면 400을 응답한다', (done) => {
       request(app)
         .get('/users?limit=two')
         .expect(400)
@@ -33,7 +33,7 @@ describe('GET /users는', () => {
 
 describe('GET /users/1은', () => {
   describe('Success!', () => {
-    it('1. id가 1인 유저 객체를 반환한다', (done) => {
+    it('id가 1인 유저 객체를 반환한다', (done) => {
       request(app)
         .get('/users/1')
         .end((err, res) => {
@@ -43,14 +43,14 @@ describe('GET /users/1은', () => {
     })
   })
   describe('Fail!', () => {
-    it('1. id가 숫자가 아닐 경우 400으로 응답', (done) => {
+    it('id가 숫자가 아닐 경우 400으로 응답', (done) => {
       request(app)
         .get('/users/one')
         .expect(400)
         .end(done)
     })
   })
-  it('2. id로 유저를 찾을 수 없을 경우 404로 응답한다', (done) => {
+  it('id로 유저를 찾을 수 없을 경우 404로 응답한다', (done) => {
     request(app)
       .get('/users/999')
       .expect(404)
@@ -60,7 +60,7 @@ describe('GET /users/1은', () => {
 
 describe('DELELTE /users/1은', () => {
   describe('성공시', () => {
-    it('1. 204를 응답한다', (done) => {
+    it('204를 응답한다', (done) => {
       request(app)
         .delete('/users/1')
         .expect(204)
@@ -68,7 +68,7 @@ describe('DELELTE /users/1은', () => {
     })
   })
   describe('실패시', () => {
-    it('1. id가 숫자가 아닐 경우 400으로 응답', (done) => {
+    it('id가 숫자가 아닐 경우 400으로 응답', (done) => {
       request(app)
         .delete('/users/one')
         .expect(400)
@@ -78,7 +78,7 @@ describe('DELELTE /users/1은', () => {
 })
 
 describe('POST /users 는', () => {
-  describe('성공시 1. 201상태코드 반환', () => {
+  describe('성공시 201상태코드 반환', () => {
     let
       body,
       name = 'daniel'
@@ -100,14 +100,14 @@ describe('POST /users 는', () => {
     })
   })
   describe('실패시', () => {
-    it('1. name 파라미터 누락시 400을 반환한다', (done) => {
+    it('name 파라미터 누락시 400을 반환한다', (done) => {
       request(app)
         .post('/users')
         .send({})
         .expect(400)
         .end(done)
     })
-    it('2. name이 중복일 경우 409를 반환한다.', (done) => {
+    it('name이 중복일 경우 409를 반환한다.', (done) => {
       request(app)
         .post('/users')
         .send({ name: 'daniel' })
@@ -128,6 +128,35 @@ describe('PUT /users/:id 는', () => {
           res.body.should.have.property('name', name)
           done()
         })
+    })
+  })
+  describe('실패시', () => {
+    it('정수가 아닌 id의 경우 400을 응답한다', (done) => {
+      request(app)
+        .put('/users/one')
+        .expect(400)
+        .end(done)
+    })
+    it('name이 없을 경우 400을 응답한다', (done) => {
+      request(app)
+        .put('/users/1')
+        .send({})
+        .expect(400)
+        .end(done)
+    })
+    it('없는 유저일 경우 404를 응답한다', (done) => {
+      request(app)
+        .put('/users/999')
+        .send({ name: 'foo' })
+        .expect(404)
+        .end(done)
+    })
+    it('이름이 중복일 경우 409를 응답한다', (done) => {
+      request(app)
+        .put('/users/3')
+        .send({ name: 'betty' })
+        .expect(409)
+        .end(done)
     })
   })
 })
