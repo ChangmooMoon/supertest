b0853df83ff93398d7ada1afae3752a7af4fb44e
moo25 <ansckdan@gmail.com>
2018-04-27 22:48:37 +0900

    router 적용하고 api 폴더로 분리

--- index.js
+++ index.js
@@ -2,70 +2,14 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-let users = [
-  { id: 1, name: 'alice' },
-  { id: 2, name: 'betty' },
-  { id: 3, name: 'chris' }
-]
+const user = require('./api/user')
 
 app.use(morgan('dev'))
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/users', (req, res) => {
-  req.query.limit = req.query.limit || 10
-  const limit = parseInt(req.query.limit, 10)
-  if (Number.isNaN(limit)) return res.status(400).end()
+app.use('/users', user)
 
-  res.json(users.slice(0, limit))
-})
-
-app.get('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10)
-  if (Number.isNaN(id)) return res.status(400).end()
-
-  const user = users.filter((user) => user.id === id)[0]
-  if (!user) res.status(404).end()
-
-  res.json(user)
-})
-
-app.delete('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10)
-  if (Number.isNaN(id)) return res.status(400).end()
-  users = users.filter(user => user.id !== id) // 조건에 맞는 새로운 배열을 원래 배열에 대입
-  res.status(204).end()
-})
-
-app.post('/users', (req, res) => {
-  const name = req.body.name
-  if (!name) return res.status(400).end()
-
-  const isConflict = users.filter(user => user.name === name).length
-  if (isConflict) return res.status(409).end()
-
-  const id = Date.now()
-  const user = { id, name }
-  users.push(user)
-  res.status(201).json(user)
-})
-
-app.put('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10)
-  if (Number.isNaN(id)) return res.status(400).end()
-
-  const name = req.body.name
-  if (!name) return res.status(400).end()
-
-  const isConflict = users.filter(user => user.name === name).length
-  if (isConflict) return res.status(409).end()
-
-  const user = users.filter(user => user.id === id)[0]
-  if (!user) return res.status(404).end()
-
-  user.name = name
-  res.json(user)
-})
 app.listen(3000, () => {
   console.log('Example app listening on port 3000')
 })
