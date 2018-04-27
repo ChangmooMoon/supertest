0a0f5dc0faff3ab93de4580024be1c524ac325cd
moo25 <ansckdan@gmail.com>
2018-04-27 18:12:39 +0900

    API test(user lookup success,fail)

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const users = [
+  { id: 1, name: 'alice' },
+  { id: 2, name: 'betty' },
+  { id: 3, name: 'chris' }
+]
+
+app.use(morgan('dev'))
+
+app.get('/users', (req, res) => {
+  req.query.limit = req.query.limit || 10
+  const limit = parseInt(req.query.limit, 10)
+  if (Number.isNaN(limit)) return res.status(400).end()
+  res.json(users.slice(0, limit))
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000')
+})
+
+module.exports = app
